eb3013d620dadc2e06bd260bbbb6e8f9a465aeb2
Hawas Muhaba <hawasmuhaba4socials@gmail.com>
2024-04-24 23:08:40 +0300

    Added the whole file

---
+++ include/header.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="stylesheet" type="text/css" href="<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">">
+	
+	<script src="https://code.jquery.com/jquery-3.7.1.slim.js" integrity= "sha256-UgvvN8vBkgO0luPSUl2s8TIlOSYRoGFAX4jlCIm9Adc=" crossorigin="anonymous"></script>
+	
+	<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.4.2/css/fontawesome.min.css">
+
+	<link rel="stylesheet" href="./css/style.css">
+	<style>
+.navbar {
+  position: relative;
+  display: -ms-flexbox;
+  display: flex;
+  -ms-flex-wrap: wrap;
+  flex-wrap: wrap;
+  -ms-flex-align: center;
+  align-items: center;
+  -ms-flex-pack: justify;
+  justify-content: space-between;
+  padding: 0.5rem 1rem;
+}
+
+.bg-info {
+  background-color: #17a2b8 !important;
+}
+.nav-link {
+  display: block;
+  padding: 0.5rem 1rem;
+}
+
+.nav-link:hover, .nav-link:focus {
+  text-decoration: none;
+}
+
+.nav-link.disabled {
+  color: #6c757d;
+  pointer-events: none;
+  cursor: default;
+}
+	</style>
+	<title>Hospital Managment system</title>
+</head>
+<body>
+
+	<nav class="navbar navbar-expand-lg bg-info">
+		<h5>Hospital Managment System</h5>
+
+		<div class = "mr-auto"></div>
+
+		<ul class="">
+			<li class= "nav-item"><a href=""  class="nav-link">Admin</a></li>
+			<li class= "nav-item"><a href=""  class="nav-link">Doctor</a></li>
+			<li class= "nav-item"><a href=""  class="nav-link">Patient</a></li>
+	</nav>
+
+</body>
+</html>
---
+++ include/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">">
+	<title></title>
+</head>
+<body>
+<?php
+include('./header.php'); 
+?>
+<div>
+	<div style="display:flex; justify-content: space-between">
+		<div>
+			
+			<img src="./img/pexels-sheinshine-3127880.jpg" width="200px">
+			<h5>Click here to get more information</h5>
+		<a>
+			<button style="margin-left: 30%;">More</button>
+		</a>
+		</div>
+		<div>
+			<img src="./img/pexels-olly-3769151.jpg" width="200px">
+			
+			<h5>Create Account so that we can take good care of you</h5>
+		<a>
+			<button style="margin-left: 30%;">Create Account!</button>
+		</a>
+		</div>
+		<div>
+			<img src="./img/pexels-rethaferguson-3825586.jpg" width="200px">
+
+		<h5>We are employing for doctors</h5>
+		<a>
+			<button style="margin-left: 30%;">Apply Now!</button>
+		</a>
+		</div>
+	</div>
+</div>
+
+</body>
+</html>
