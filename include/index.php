d4d27e89a588a264ad976a7753b6ad27b46a9d30
Hamzshaa <trident32000@gmail.com>
2024-04-28 10:45:10 +0300

    css and images changed

--- include/index.php
+++ include/index.php
@@ -3,38 +3,37 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" type="text/css" href="<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">">
+	<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+	 <link rel="stylesheet"  href="./include/css/index.css?v=<?php echo time(); ?>">
+    
 	<title></title>
 </head>
 <body>
 <?php
-include('./header.php'); 
+include('./include/header.php'); 
 ?>
 <div>
-	<div style="display:flex; justify-content: space-between">
+	<div  class="call-to-action-wrapper">
 		<div>
-			
-			<img src="./img/pexels-sheinshine-3127880.jpg" width="200px">
+			<img src="./include/img/more-info.png">
 			<h5>Click here to get more information</h5>
-		<a>
-			<button style="margin-left: 30%;">More</button>
-		</a>
+			<a>
+				More
+			</a>
 		</div>
 		<div>
-			<img src="./img/pexels-olly-3769151.jpg" width="200px">
-			
+			<img src="./include/img/create-account.png">
 			<h5>Create Account so that we can take good care of you</h5>
-		<a>
-			<button style="margin-left: 30%;">Create Account!</button>
-		</a>
+			<a>
+				Create Account!
+			</a>
 		</div>
 		<div>
-			<img src="./img/pexels-rethaferguson-3825586.jpg" width="200px">
-
-		<h5>We are employing for doctors</h5>
-		<a>
-			<button style="margin-left: 30%;">Apply Now!</button>
-		</a>
+			<img src="./include/img/employee-doctors.png">
+			<h5>We are employing for doctors</h5>
+			<a>
+				Apply Now!
+			</a>
 		</div>
 	</div>
 </div>
